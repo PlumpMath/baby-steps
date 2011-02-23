de27bdfad25665325b9acfc1860b8a826e8adcc2
aerique <aerique@xs4all.nl>
2011-02-23 12:48:01 +0100

    Add ramped-half-and-half, full and grow methods.

--- src/creation.lisp
+++ src/creation.lisp
@@ -16,7 +16,8 @@
 
 
 (defun create-initial-population (operators fitness-fn test-input
-                                  &optional (size 100))
+                                  &key (max-depth 4) (method :ramped-half-half)
+                                       (size 100))
   (let ((population (make-instance 'population
                                    :fitness-fn fitness-fn
                                    :motes (make-array 0 :fill-pointer 0)
@@ -25,7 +26,13 @@
                                    :test-input test-input)))
     (loop with n-motes = 0
           until (>= n-motes size)
-          for mote = (create-mote operators fitness-fn test-input)
+          ;; always ramp up to max-depth from 0
+          for cm-max-depth = (ceiling (* max-depth (/ n-motes size)))
+          for cm-method = (if (equal method :ramped-half-half)
+                              (if (evenp n-motes) :full :grow)
+                              method)
+          for mote = (create-mote operators fitness-fn test-input
+                                  :max-depth cm-max-depth :method cm-method)
           when (fitness mote)
           do (vector-push-extend mote (motes population))
              (incf n-motes))
@@ -33,10 +40,12 @@
     population))
 
 
-(defun create-mote (operators fitness-fn test-input &key (debug nil))
+(defun create-mote (operators fitness-fn test-input
+                    &key (debug nil) (max-depth 4) (method :grow))
   "Creates a new MOTE instance with a fitness guaranteed not be NIL."
   (loop for i from 1
-        for rtree = (create-random-tree operators)
+        for rtree = (create-random-tree operators :max-depth max-depth
+                                        :method method)
         for fitness = (calculate-fitness rtree fitness-fn test-input)
         until fitness
         finally (when debug
@@ -47,7 +56,30 @@
                                              :tree rtree))))
 
 
-(defun create-random-tree (operators &key (max-depth 4))
+(defun create-random-tree (operators &key (max-depth 4) (method :grow))
+  "Generates a random tree using OPERATORS.  MAX-DEPTH must either be an
+  integer equal to or greater than 0 or :UNLIMITED.  The latter is not
+  recommended and only possible for the :GROW method.
+  METHOD must be eithet :FULL or :GROW."
+  (case method
+    (:full (create-random-tree-full operators :max-depth max-depth))
+    (:grow (create-random-tree-grow operators :max-depth max-depth))))
+
+
+(defun create-random-tree-full (operators &key (max-depth 4))
+  "Generates a random tree using OPERATORS.  MAX-DEPTH must be an integer equal
+  to or greater than 0."
+  (append (list (random-elt operators))
+          (loop repeat 2
+                collect (if (> max-depth 0)
+                            (create-random-tree-full operators
+                                                    :max-depth (- max-depth 1))
+                            (if (= 0 (random 2))
+                                (random 10.0)
+                                '=input=)))))
+
+
+(defun create-random-tree-grow (operators &key (max-depth 4))
   "Generates a random tree using OPERATORS.  MAX-DEPTH must either be an
   integer equal to or greater than 0 or :UNLIMITED.  The latter is not
   recommended."
@@ -55,7 +87,7 @@
           (loop repeat 2
                 collect (if (or (> max-depth 0) (equal max-depth :unlimited))
                             (if (= 0 (random 2))
-                                (create-random-tree operators
+                                (create-random-tree-grow operators
                                                     :max-depth (- max-depth 1))
                                 (if (= 0 (random 2))
                                     (random 10.0)
